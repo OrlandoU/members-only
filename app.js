d44eb4f4c1e3e1bcfeca3ce5bbbfd4e051e63c81
Orlando Umanzor <orlandojose729@gmail.com>
2023-04-17 15:49:01 -0600

    Finish setting up the project

--- app.js
+++ app.js
@@ -52,12 +52,15 @@ passport.deserializeUser(async (id, done)=>{
   };
 })
 
-app.use(passport.session())
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use('/', indexRouter);
 app.use('/post', postRouter)
